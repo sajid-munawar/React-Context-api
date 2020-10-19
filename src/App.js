d83676d639550016fea9fd111a6f054f280914ab
sajid507 <sajid92500@gmail.com>
2020-10-19 21:38:01 +0500

    remove some files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Parent from "./Parent.js"
+// import Parent from "./Parent.js"
 import './App.css';
 import CounterContext from "./CounterContext"
 import Another from "./Another"
