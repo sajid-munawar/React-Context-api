2adc90d5724746c94bc1264522fb41f13c0c6144
sajid507 <sajid92500@gmail.com>
2020-10-19 21:53:56 +0500

    Just an html to check auto deploy

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 // import Parent from "./Parent.js"
 import './App.css';
-import CounterContext from "./CounterContext"
-import Another from "./Another"
+// import CounterContext from "./CounterContext"
+// import Another from "./Another"
 
 function App() {
-  let countState=useState(10) // c[count , set]
+  // let countState=useState(10) // c[count , set]
   return (
-    <CounterContext.Provider value={countState}>
+    // <CounterContext.Provider value={countState}>
       <div >
-        <Another/>
+        This is simple react App
+        {/* <Another/> */}
 
         {/* <Parent /> */}
       </div>
-    </CounterContext.Provider>
+    // </CounterContext.Provider>
   );
 }
 
