9aa652083eb56430aebeb01d5499c77233bb55a6
sajid507 <sajid92500@gmail.com>
2020-10-19 23:06:06 +0500

    Update CounterReducer.js

--- src/CounterReducer.js
+++ src/CounterReducer.js
@@ -1,10 +1,10 @@
-const counterReducer = (state, action) => {
-  switch (action) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-  }
+function counterReducer(state, action) {
+    switch (action) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+    }
 }
 
 export default counterReducer;
